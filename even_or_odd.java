4e9b73ade703d4debc766ea5c4414ea40a58f115
heba-jalil <163648306+heba-mustafa59@users.noreply.github.com>
2024-04-13 12:53:13 +0300

    Add files via upload

---
+++ even_or_odd.java
@@ -0,0 +1,21 @@
+package q1;
+
+import java.util.Scanner;
+
+public class Q1 {//heba jalil
+
+     public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        int n;
+        n=in.nextInt();
+        if (n%2==0){
+            System.out.println("even number");
+        }
+        else if (n%2!=0){
+            System.out.println("odd number");
+        }
+     }
+
+       
+}
+
